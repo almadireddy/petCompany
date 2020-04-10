6d0c4e4abfb4757564fef22c226963702d613acf
Al Madireddy <al.madireddy@gmail.com>
2020-04-09 20:01:41 -0500

    added shifts and stuff

--- PET_SALON_CREATE.sql
+++ PET_SALON_CREATE.sql
@@ -22,13 +22,13 @@ CREATE TABLE STORE_PRODUCTS(
     Brand VARCHAR(255) NOT NULL,
     Price FLOAT(2),
     CONSTRAINT pk_storeproducts PRIMARY KEY (ProductID, quantity)
-    );
+);
     
 DROP TABLE IF EXISTS SPECIES;
 CREATE TABLE SPECIES(
-	Name VARCHAR(255) NOT NULL,
+    Name VARCHAR(255) NOT NULL,
     CONSTRAINT pk_species PRIMARY KEY (Name)
-    );
+);
     
 DROP TABLE IF EXISTS PRODUCT_SPECIES;
 CREATE TABLE PRODUCT_SPECIES(
@@ -36,7 +36,7 @@ CREATE TABLE PRODUCT_SPECIES(
     Species VARCHAR(255) NOT NULL,
     CONSTRAINT fk_productspecies_storeproducts FOREIGN KEY (StoreProduct) references STORE_PRODUCTS(ProductID), 
 	CONSTRAINT fk_productspecies_species FOREIGN KEY (Species) references SPECIES (Name) 
-    );
+);
 
 DROP TABLE IF EXISTS PET;
 CREATE TABLE PET(
@@ -79,4 +79,42 @@ CREATE TABLE RECEPTIONIST(
 	ReceptionistID	INT NOT NULL,
     CONSTRAINT pk_receptionist PRIMARY KEY (ReceptionistID),
     CONSTRAINT fk_receptionist_employee FOREIGN KEY (ReceptionistID) references EMPLOYEE (EmployeeID)
+);
+
+DROP TABLE IF EXISTS SHIFT;
+CREATE TABLE SHIFT(
+    ShiftID int not null PRIMARY KEY,
+    shift_type VARCHAR(2) not null,
+    date datetime not null,
+    supervisor int not null,
+    receptionist int not null,
+    CONSTRAINT fk_shift_supervisor FOREIGN KEY (supervisor)
+        REFERENCES MANAGER(ManagerID),
+    CONSTRAINT fk_shift_receptionist FOREIGN KEY (receptionist)
+        REFERENCES RECEPTIONIST(ReceptionistID)
+);
+
+DROP TABLE IF EXISTS GROOMER_SHIFTS;
+CREATE TABLE GROOMER_SHIFT(
+    groomer_id int not null,
+    shift_id int not null,
+    CONSTRAINT pk_groomer_shift_id PRIMARY KEY (groomer_id, shift_id),
+    CONSTRAINT fk_groomer_shift_groomer FOREIGN KEY (groomer_id) 
+        REFERENCES GROOMER(GroomerID),
+    CONSTRAINT fk_groomer_shift_shift FOREIGN KEY (shift_id) 
+        REFERENCES SHIFT(ShiftID)
+);
+
+DROP TABLE IF EXISTS APPOINTMENT;
+create table APPOINTMENT(
+	appointment_id int not null primary key,
+    duration time not null,
+    start_time timestamp not null,
+    groomer_id int not null,
+    owner_id int not null,
+    pet_name varchar(255) not null,
+    constraint fk_appointment_groomer foreign key (groomer_id)
+		references GROOMER(GroomerID),
+	constraint fk_appointment_owner_pet foreign key (owner_id, pet_name)
+		references PET(OwnerId, Name)
 );
