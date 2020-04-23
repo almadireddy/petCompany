e9ca4a0cb90fd8edcbe76955d3b46072bf1c515f
Arjun Khurana <arjun@Arjuns-MacBook-Pro.local>
2020-04-23 17:21:29 -0500

    generated sample pet_insert sql

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import numpy as np
 
-def generate(names, breeds):
+def generate_pet(names, breeds):
     name = np.random.choice(names) 
     name = name if name[0] != '*' else name[1:]
     breed = breeds[np.random.choice(len(breeds))]
     return [name, breed[0], breed[1]]
 
-data = pd.read_csv('datasets/petdata.csv').set_index('Animal ID')
+pet_data = pd.read_csv('datasets/petdata.csv').set_index('Animal ID')
 
-names = list(data['Pet name'])
-breeds = list(zip(data['Animal Type'], data['Breed']))
+names = list(pet_data['Pet name'])
+breeds = list(zip(pet_data['Animal Type'], pet_data['Breed']))
 
-with open('test.sql', 'w') as file:
+with open('pet_insert.sql', 'w') as file:
     for x in range(1000):
-        file.write('INSERT INTO PET (Name, Breed, Species) VALUES ({0}, {1}, {2});\n'.format(*generate(names, breeds)))
+        file.write('INSERT INTO PET (Owner_id, Name, Breed, Species) VALUES ({0}, {1}, {2}, {3});\n'.format(123, *generate_pet(names, breeds)))
